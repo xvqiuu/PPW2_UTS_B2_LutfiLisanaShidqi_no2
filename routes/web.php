036009ac383ecfa81063e3ef98f41a4b28a11d04
xvqiuu <132545168+xvqiuu@users.noreply.github.com>
2023-10-10 14:05:12 +0700

    gfnhhf

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('/products', [Controller::class,'index']);
+
+// Route untuk menampilkan daftar produk
+Route::get('/', [Controller::class],'index')->name('index');
+
+// Route untuk menampilkan form tambah produk
+Route::get('/products/create', [Controller::class],'create')->name('products.create');
+
+// Route untuk menyimpan data produk baru
+Route::post('/products', [Controller::class],'store')->name('products.store');
+
+// Route untuk menampilkan detail produk
+Route::get('/products/{id}', [Controller::class],'show')->name('products.show');
+
+// Route untuk menampilkan form edit produk
+Route::get('/products/{id}/edit', [Controller::class],'edit')->name('products.edit');
+
+// Route untuk mengupdate data produk
+Route::put('/products/{id}',[Controller::class],'update')->name('products.update');
+
+// Route untuk menghapus produk
+Route::delete('/products/{id}', [Controller::class],'destroy')->name('products.destroy');
